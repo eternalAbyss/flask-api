5f104a8a7428a58265ab0cc534f3cd54dd502658
Utkarsh Singh <us9044@gmail.com>
2023-10-16 11:43:35 +0530

    Added comments and docstrings for the test case

--- flask_application/app.py
+++ flask_application/app.py
@@ -4,10 +4,12 @@ import logging
 
 app = Flask(__name__)
 
+# Basic Route for testing
 @app.route('/')
 def hello():
     return {'msg':'hello world!'}
 
+# Route to log the thread number and the number of threads running
 @app.route('/thread')
 def threading_count():
     logging.info("Number of threads running currently : {}".format(threading.active_count()))
--- flask_application/test/test_conc_requests.py
+++ flask_application/test/test_conc_requests.py
@@ -3,23 +3,31 @@ import logging
 import time
 
 def test_concurrent_requests():
+    '''
+    Test case to check for the concurrency in the flask application.
+    Number of requests can be altered to match the system configurations.
+    '''
     urls = {
         'url1': '127.0.0.1:5000/thread',
         'url2': '127.0.0.1:5000/'
     }
+    # Single call metric calculation
     st_time = time.time()
-    cmd = "curl {} ".format(urls["url1"])
+    cmd = "curl {} ".format(urls["url1"])   # Command to curl the request to the flask application
     res = os.system(cmd)
     end_time = time.time()
-    time_elapsed = round(end_time - st_time, 4)
+    time_elapsed = round(end_time - st_time, 4)     # Total time elapsed for the single call
+
+    # Concurrent call metric calculation
     st_time = time.time()
-    request_count = 500
-    con_cmd = '& '.join([cmd for i in range(request_count)])
-    con_res = os.system(con_cmd)
+    request_count = 500     # Total number of requests to send to the flask application
+    con_cmd = '& '.join([cmd for i in range(request_count)])    # Creating curl command to make concurrent requests to the flask application
+    con_res = os.system(con_cmd)    # Making the concurrent calls
     end_time = time.time()
-    con_time_elapsed = round(end_time - st_time, 4)
-    print(time_elapsed)
-    print(con_time_elapsed)
-    concurrency_rate = (con_time_elapsed/time_elapsed) 
+    con_time_elapsed = round(end_time - st_time, 4)     # Total time taken for the concurrent requests to go through
+    # Printing the respective times
+    print("Time taken for single request : ".format(time_elapsed))
+    print("Time taken for concurrent {} requests : ".format(request_count, con_time_elapsed))
+    concurrency_rate = (con_time_elapsed/time_elapsed)      # Rate of concurrent requests getting processed
     logging.info("Concurrency Rate : {}".format(concurrency_rate))
     assert concurrency_rate > 2
